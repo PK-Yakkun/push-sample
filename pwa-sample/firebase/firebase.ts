26e5fea15e83a63e32698c722df19be056317907
PK-Yakkun <mokkun515@gmail.com>
2023-04-09 18:29:21 +0900

    add: firebase SDKでトークン取得処理の実装

---
+++ pwa-sample/firebase/firebase.ts
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getMessaging, getToken } from "firebase/messaging";
+
+const api_key = process.env.NEXT_PUBLIC_FIREBASE_API_KEY;
+const auth_domain = process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN;
+const project_id = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
+const storage_bucket = process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET;
+const messaging_sender_id =
+  process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID;
+const app_id = process.env.NEXT_PUBLIC_FIREBASE_APP_ID;
+const measurement_id = process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID;
+const vapid_key = process.env.NEXT_PUBLIC_FIREBASE_VAPID_KEY;
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: api_key,
+  authDomain: auth_domain,
+  projectId: project_id,
+  storageBucket: storage_bucket,
+  messagingSenderId: messaging_sender_id,
+  appId: app_id,
+  measurementId: measurement_id,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const messaging = getMessaging(app);
+
+getToken(messaging, { vapidKey: vapid_key });
